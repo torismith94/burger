79ad72a1803bffe5151c9d0ac2f67798533aa6d1
Victoria Smith <torismith@Victorias-MacBook-Pro-3.local>
2017-12-09 10:08:42 -0800

    server changes

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
+
 // var connection = require('./config/connection');
 
 var orm = require('./config/orm');
